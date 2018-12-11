f3f32b3ed69362fb1c56e9b343ab53b8ab5bc5b2
pranaymokal <pranay.mokal@gmail.com>
2018-12-11 14:46:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+//let DEV_DB = 'mongodb://root:root1234@ds127634.mlab.com:27634/mongo';
+//const mongoDB = process.env.MONGODB_URI || DEV_DB;
+
+let DEV_DB = 'mongodb://localhost:27017/mongo';
+mongoose.connect(DEV_DB, { useNewUrlParser: true });
+
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const userRoute=require('./src/routes/user.route');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/users',userRoute);
+let port = 3000;
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
